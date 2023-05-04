00356525a9d162504df4c4bbde6c38aaad8b7e02
hyunhee <nnhhnn9592@gmail.com>
2023-05-04 16:15:09 +0900

    20230504

---
+++ 20230504/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TripList from './components/tripList/TripList';
+
+function App() {
+  return (
+    <div>
+      <TripList />
+    </div>
+  );
+}
+
+export default App;
---
+++ 20230504/my-app/src/components/newMenu/NewMenu.js
@@ -0,0 +1,11 @@
+import './NewMenu.css';
+
+function NewMenu() {
+  return (
+    <ul className="newClass">
+      <li>초밥</li>
+      <li>맥모닝</li>
+    </ul>
+  );
+}
+export default Menu;
