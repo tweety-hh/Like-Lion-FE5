9cd22868a0cf2e58e7360094d45b6b761c4206b1
hyunhee <nnhhnn9592@gmail.com>
2023-05-08 17:21:26 +0900

    20230508 React

---
+++ 20230508/makecomponents/src/Components/Hello.jsx
@@ -0,0 +1,13 @@
+function Hello(props) {
+  const name = '라이캣!';
+  const someStyle = { backgroundColor: 'black', color: 'white' };
+  return (
+    <div>
+      <h1 style={someStyle}>안녕, {props.name} 1호</h1>
+      <h1>안녕, 라이캣 2호!</h1>
+      <div className="newClass" />
+    </div>
+  );
+}
+
+export default Hello;
