1e6b4ddd8d74939c5c4d3dee85097ac496745719
hyunhee <nnhhnn9592@gmail.com>
2023-06-08 20:15:53 +0900

    fire base 미완

--- 20230607/mydiary/src/hooks/useLogin.js
+++ 20230607/mydiary/src/hooks/useLogin.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { appAuth } from '../firebase/config';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useAuthContext } from './useAuthContext';
 
 // 회원가입을 진행하는 훅
-export const useSignup = () => {
+export const useLogin = () => {
   // 에러 정보를 저장합니다.
   const [error, setError] = useState(null);
   // 현재 서버와의 통신 상태를 저장합니다.
@@ -12,32 +12,23 @@ export const useSignup = () => {
 
   const { dispatch } = useAuthContext();
 
-  const signup = (email, password, displayName) => {
+  const login = (email, password) => {
     setError(null);
     setPending(true);
 
     // 회원가입이 처리되는 함수
-    createUserWithEmailAndPassword(appAuth, email, password)
+    signInWithEmailAndPassword(appAuth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
         console.log(user);
-        if (!user) {
-          throw new Error('회원 가입에 실패했습니다!');
-        }
+        dispatch({ type: 'login', payload: user });
 
-        // 회원의 별명정보를 업데이트합니다.
-        updateProfile(appAuth.currentUser, { displayName })
-          .then(() => {
-            setError(null);
-            setPending(false);
-            dispatch({ type: 'login', payload: user });
-          })
-          .catch((err) => {
-            setError(err.message);
-            setPending(false);
-            console.log(err.message);
-          });
+        if (!user) {
+          throw new Error('로그인에 실패했습니다!');
+        }
+        setError(null);
+        setPending(false);
       })
       .catch((err) => {
         setError(err.message);
@@ -45,5 +36,5 @@ export const useSignup = () => {
         console.log(err.message);
       });
   };
-  return { error, isPending, signup };
+  return { error, isPending, login };
 };
--- 20230607/mydiary/src/pages/home/Home.jsx
+++ 20230607/mydiary/src/pages/home/Home.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styles from './Home.module.css';
+import DiaryForm from './DiaryForm';
 
 export default function Home() {
-  return <div>Home</div>;
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return (
+    <div className={styles.container}>
+      <main className={styles['diary-main']}>
+        <h2 className={styles.heart}>2023.02.27의 비밀일기</h2>
+        <DiaryForm />
+      </main>
+      <section>
+        <h2 className="a11y-hidden">일기 목록</h2>
+        <ul></ul>
+      </section>
+    </div>
+  );
 }
