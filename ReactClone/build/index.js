6193f78b86a0e804e230fa0641e80a3f0a9124be
hyunhee <nnhhnn9592@gmail.com>
2023-07-26 22:44:53 +0900

    20230726 React Clone

---
+++ ReactClone/build/index.js
@@ -0,0 +1,9 @@
+/* @jsx createElement */
+import { createElement, render } from './react.js';
+function Title() {
+  return createElement("div", null, createElement("h1", {
+    className: "title"
+  }, "hello react title"), createElement("strong", null, "gooooooodd!!!!"));
+}
+console.log(Title());
+render(createElement(Title, null), document.querySelector('#root'));
